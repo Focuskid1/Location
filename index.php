29b6faba7a22e7e17dd1aa97b6738490cb848d02
Focuskid1 <168267184+Focuskid1@users.noreply.github.com>
2024-09-23 15:22:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,5 @@ fwrite($myfile, $txt);
 // Close the file
 fclose($myfile);
 
-echo "User information stored successfully!<br><br>";
-
-// Now, read and display the content of the location.txt file
-$filename = 'location.txt';
-
-// Check if the file exists before trying to read it
-if (file_exists($filename)) {
-    // Read and output the file content
-    $content = file_get_contents($filename);
-    echo nl2br($content);  // nl2br converts newlines to <br> tags for proper formatting in HTML
-} else {
-    echo "The file does not exist.";
-}
+echo "User information stored successfully!";
 ?>
